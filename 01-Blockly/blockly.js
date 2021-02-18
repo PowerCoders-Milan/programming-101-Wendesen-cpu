2313b10e8a46d706a0af1b0a1d2ef607214b9037
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-18 11:45:05 +0100

    Finished till level 9

--- 01-Blockly/blockly.js
+++ 01-Blockly/blockly.js
@@ -1,1 +1,64 @@
 //Insert you code below
+moveForward();
+moveForward();
+moveForward();
+
+turnLeft();
+moveForward();
+turnRight();
+moveForward();
+
+while (notDone()) {
+    moveForward();
+  }
+
+  while (notDone()) {
+    moveForward();
+    turnLeft();
+    moveForward();
+    turnRight();
+  }
+
+  moveForward();
+moveForward();
+turnLeft();
+while (notDone()) {
+  moveForward();
+}
+
+while (notDone()) {
+    moveForward();
+    if (isPathLeft()) {
+      turnLeft();
+    }
+  }
+
+
+  while (notDone()) {
+    moveForward();
+    if (isPathRight()) {
+      turnRight();
+    }
+  }
+
+  while (notDone()) {
+    moveForward();
+    if (isPathLeft()) {
+      turnLeft();
+    }
+    if (isPathRight()) {
+      turnRight();
+    }
+  }
+
+  while (notDone()) {
+    if (isPathForward()) {
+      moveForward();
+    } else {
+      if (isPathLeft()) {
+        turnLeft();
+      }
+      if (isPathRight()) {
+        turnRight();
+      }
+    }

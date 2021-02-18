267e98278d6c6c2357e675d171404b69ba7ccdb5
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-18 18:02:15 +0100

    Final version

--- 02-Sorting-Numbers/sorting_numbers.js
+++ 02-Sorting-Numbers/sorting_numbers.js
@@ -1,6 +1,23 @@
-//insert your pseudocode below
-/*
-here
-*/
+
 
 //insert your code below
+
+function listsGetSortCompare(type, direction) {
+    var compareFuncs = {
+      "NUMERIC": function(a, b) {
+          return Number(a) - Number(b); },
+      "TEXT": function(a, b) {
+          return a.toString() > b.toString() ? 1 : -1; },
+      "IGNORE_CASE": function(a, b) {
+          return a.toString().toLowerCase() > b.toString().toLowerCase() ? 1 : -1; },
+    };
+    var compare = compareFuncs[type];
+    return function(a, b) { return compare(a, b) * direction; }
+  }
+  
+  
+  window.alert([window.prompt('Insert the number'), window.prompt('Insert the second number'), window.prompt('Insert the third number')].slice().sort(listsGetSortCompare("NUMERIC", 1)));
+  
+  [].slice().sort(listsGetSortCompare("NUMERIC", 1));
+  
+  window.prompt('abc');

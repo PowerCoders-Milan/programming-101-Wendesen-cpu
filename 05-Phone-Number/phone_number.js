231f72c1349aa3c311016b6c69a57ea418f97e1e
Wendesen Melaku Belay <w.belay@studenti.unibs.it>
2021-02-23 17:19:19 +0100

    Done

--- 05-Phone-Number/phone_number.js
+++ 05-Phone-Number/phone_number.js
@@ -4,3 +4,30 @@ here
 */
 
 //insert your code below
+
+var result2, result, input, i;
+
+
+result2 = '';
+result = '';
+input = window.prompt('Input the phone number').split(' ');
+for (var i_index in input) {
+  i = input[i_index];
+  result += String(i);
+}
+input = result.split('.');
+for (var i_index2 in input) {
+  i = input[i_index2];
+  result2 += String(i);
+}
+if (result2.length == 15) {
+  if (result2.charAt(0) == '(') {
+    window.alert('Valid Phone number');
+  } else {
+    window.alert('Invalid Phone number');
+  }
+} else if (result2.length == 10) {
+  window.alert('Valid Phone number');
+} else {
+  window.alert('Invalid Phone number');
+}
